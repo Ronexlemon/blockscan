6a4e05556e33d0a6cf9f0e42ff852ce122b33808
ronexlemon <ronexondimu@gmail.com>
2026-04-12 19:36:52 +0300

    ft:block decoder

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,1 +1,9 @@
 #bash
+
+docker run --name postgres-db \
+  -e POSTGRES_USER=myuser \
+  -e POSTGRES_PASSWORD=mypassword \
+  -e POSTGRES_DB=mydb \
+  -p 5432:5432 \
+  -v pgdata:/var/lib/postgresql/data \
+  -d postgres:16
